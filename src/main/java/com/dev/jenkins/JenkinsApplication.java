b00887970d7d37928df41c70741719cb69a99862
Mohsin Shabbir <link2mohsin22@gmail.com>
2024-03-19 11:50:51 +0700

    - second commit

--- src/main/java/com/dev/jenkins/JenkinsApplication.java
+++ src/main/java/com/dev/jenkins/JenkinsApplication.java
@@ -13,7 +13,7 @@ public class JenkinsApplication {
 
 	@PostConstruct
 	public  void init(){
-		logger.info("Application Started...");
+		logger.info("Application Started...Mohsin");
 	}
 	public static void main(String[] args) {
 		logger.info("Application Executed...");
